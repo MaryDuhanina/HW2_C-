978cc6869fa401340d5bd3c5309779807ffacded
Mary Duhanina <maryduhanina@gmail.com>
2022-09-03 13:38:16 +0300

    Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > 99 && numberA < 1000)
+{
+    int result1 = numberA % 10;
+    Console.WriteLine("Третья цифра " + result1);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
